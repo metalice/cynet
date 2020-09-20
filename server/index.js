d84bf7a2bd0c610c62f53ba181aec3607bfe2551
Matan Schatzman <matan.schatzman@brightmachines.com>
2020-09-20 19:55:23 +0300

    added support for heruku

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const path = require("path");
 const express = require("express");
 const app = express();
 const publicPath = path.join(__dirname, "..", "build");
-const port = process.env.PORT || 80;
+const port = 80;
 app.use(express.static(publicPath));
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
