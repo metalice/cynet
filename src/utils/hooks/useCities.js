79d6d6d87c308d9c8efec6530b94df3facfc0ebf
Matan Schatzman <matan.schatzman@brightmachines.com>
2020-09-20 20:01:29 +0300

    changed request to https:

--- src/utils/hooks/useCities.js
+++ src/utils/hooks/useCities.js
@@ -3,7 +3,7 @@ import { fetcher } from "utils/communication";
 
 const API = process.env.REACT_APP_WEATHER_API;
 const cities = process.env.REACT_APP_CITIES;
-const url = `http://api.openweathermap.org/data/2.5/group?id=${cities}&units=metric&appid=${API}`;
+const url = `https://api.openweathermap.org/data/2.5/group?id=${cities}&units=metric&appid=${API}`;
 
 export const useCities = () => {
   const { data, error } = useSWR(url, fetcher, { refreshInterval: 10000 });
